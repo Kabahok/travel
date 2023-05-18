72a5131aa661a35bba16f5b131a8193d0cdc9060
egor avdeev <workegoravd@gmail.com>
2023-05-18 23:22:16 +0300

    connected the data sending form

--- js/script.js
+++ js/script.js
@@ -11,3 +11,30 @@ const swiper = new Swiper('.swiper', {
       },
 
 });
+
+function sendForm() {
+    let modalForm = document.querySelector(".form__contacts");
+
+    async function postData(url, data) {
+        const rec = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(data)
+        })
+
+        return rec.text()
+    }
+
+    modalForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+
+        let data = new FormData(modalForm);
+
+        postData("server.com", data).then((rec) => {
+            console.log(rec);
+            console.log("work");
+        }).catch(() => console.log("error")).finally(() => console.log("script work"))
+    })
+}
+
+sendForm();
