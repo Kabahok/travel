e79e87ff44f116ca8d845c5324d238d29b5873f6
egor avdeev <workegoravd@gmail.com>
2023-05-18 23:13:58 +0300

    add swiper

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+
+    slidesPerView: 3,
+    spaceBetween: 10,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+});
